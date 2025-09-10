6c77fdbae2451112a746bf377a953bbfb146bb32
Satchel R Diaz <satchelrdiaz05@gmail.com>
2025-09-10 17:39:54 -0400

    Fixing bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "greeting_utils.h"
 
-using std::cout, std::endl, std::getline, std::string;
+using std::cin, std::cout, std::endl, std::getline, std::string;
 
 int main(){
 	cout << "Enter your name: ";
 	string input;
 	cin >> input;
 	string greeting = GreetingUtils::create_message(input);
-	char* c_string = GreetingUtils::create_message(greeting);
+	char* c_string = GreetingUtils::format_as_c_string(greeting);
 	cout << c_string << endl;
 	delete[] c_string;
 	return 0;
